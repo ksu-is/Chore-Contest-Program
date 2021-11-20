fef026af6e5a9131dccab19424e2979fcf905788
Maffy0201 <89150397+Maffy0201@users.noreply.github.com>
2021-11-20 14:50:12 -0500

    Update number of items.py
    Added "\n" between contestant names to make it easier to see all names. Fixed the coding for obtaining the amount of names. Before it was giving me a weird error that my indent was wrong no matter what I did so I made it into a function and added more parameters. Also I added more ways for incorrect entries to get feedback, most of which allow the user to start over automatically.

--- number of items.py
+++ number of items.py
@@ -1,43 +1,53 @@
 def two_cont():
-        one_name = input("Name of contestant: ")
+        one_name = input("Name of 1st contestant: ")
         two_name = input("Name of 2nd contestant: ")
-        print("Contestants in this chore contest are: ", one_name, two_name)
+        print("Contestants in this chore contest are: \n", one_name, "\n", two_name)
 
 def three_cont():
-        one_name = input("Name of contestant: ")
+        one_name = input("Name of 1st contestant: ")
         two_name = input("Name of 2nd contestant: ")
         three_name = input("Name of 3rd contestant: ")
-        print("Contestants in this chore contest are: ", one_name, two_name, three_name)
+        print("Contestants in this chore contest are: \n", one_name, "\n", two_name, "\n", three_name)
 
 def four_cont():
-        one_name = input("Name of contestant: ")
+        one_name = input("Name of 1st contestant: ")
         two_name = input("Name of 2nd contestant: ")
         three_name = input("Name of 3rd contestant: ")
         four_name = input("Name of 4th contestant: ")
-        print("Contestants in this chore contest are: ", one_name, two_name, three_name, four_name)
+        print("Contestants in this chore contest are: \n", one_name, "\n", two_name, "\n", three_name, "\n", four_name)
 
 def five_cont():
-        one_name = input("Name of contestant: ")
+        one_name = input("Name of 1st contestant: ")
         two_name = input("Name of 2nd contestant: ")
         three_name = input("Name of 3rd contestant: ")
         four_name = input("Name of 4th contestant: ")
         five_name = input("Name of 5th contestant: ")
-        print("Contestants in this chore contest are: ", one_name, two_name, three_name, four_name, five_name)
+        print("Contestants in this chore contest are: \n", one_name, "\n", two_name, "\n", three_name, "\n", four_name, "\n", five_name)
         
-name_amount = input("How many contestants are in this contest? Choose a number between 2 and 5. ")
-while name_amount.isnumeric():
-	if name_amount<2:
-		print("Sorry, there are not enough people for this contest.")
-	elif name_amount>5:
-                print("There are too many contestants. You will have to divide into smaller groups to play.")
-        else:
-                if name_amount==2:
-                        two_cont()
-                elif name_amount==3:
-                        three_cont()
-                elif name_amount==4:
-                        four_cont()
+def num_names():
+        name_count = input("How many contestants are in this contest? Choose a number between 2 and 5. ")
+        if name_count.isdigit():
+                name_amount = int(name_count)
+                if name_amount<6:
+                        if name_amount<2:
+                                print("Sorry, there are not enough people for this contest.")
+                        else:
+                                if name_amount==2:
+                                        two_cont()
+                                elif name_amount==3:
+                                        three_cont()
+                                elif name_amount==4:
+                                        four_cont()
+                                else:
+                                        five_cont()
+                elif name_amount>5:
+                        print("Too many contestants. Please break them into smaller groups.\n")
+                        num_names()
                 else:
-                        five_count()
+                        print("invalid input")
+        else:
+                print("invalid input. Please type a number such as \"2\" rather than \"two\".\n")
+                num_names()
+num_names()
 
         
