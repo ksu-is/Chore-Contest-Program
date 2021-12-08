07c803603732acbf27b2ef73d53cf11a720be033
Maffy0201 <89150397+Maffy0201@users.noreply.github.com>
2021-12-08 15:28:33 -0500

    Update making_lists.py
    Changed it so that it looked nicer and worked better to make a nice list.

--- making_lists.py
+++ making_lists.py
@@ -1,23 +1,23 @@
 chore_list = ["litterboxes", "trash", "dust"]
 name_list = ["Bob", "Sally"]
 
-
-
-
+             
 first_list = []
 first_chore_count = 0
 
 for chore in chore_list:
+    print("How many times did", name_list[0], "do", chore, "?")
     chore = chore.capitalize()+":"
     first_list.append(chore)
-    chore_num = input("enter")
+    chore_num = input()
     counting = int(chore_num)
     first_chore_count += counting
     chore_num = str(counting)
     first_list.append(chore_num)
     
     
-print(name_list[0]+"'s chores completed:\n")
+print("\n"+name_list[0]+"'s chores completed:\n")
 
 print("\n".join(first_list))
-print("\nTotal chore count for ", name_list[0], "is:\n", first_chore_count)
+print("\nTotal chore count for ", name_list[0], "is:", first_chore_count)
+
